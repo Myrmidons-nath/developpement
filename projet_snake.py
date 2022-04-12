5fe23de1520990a65421aac5be4493721208f0b2
Myrmidons-nath <tueronathan@gmail.com>
2022-04-12 17:55:28 +0200

    apprentissage

---
+++ projet_snake.py
@@ -0,0 +1,43 @@
+
+from math import fabs
+from tkinter import *
+import random
+
+
+jeu_longueur=700
+jeu_largeur=700
+speed=50
+espace=50
+corps = 3
+couleur_serpend = "#00FF00"
+couleur_bouffe = "FF0000"
+couleur_fond = "000000"
+
+
+
+class serpent:
+    pass
+class bouffe:
+    pass
+
+def prochain_virage():
+    pass
+def change_direction():
+    pass
+def check_collision():
+    pass
+
+def fin():
+    pass
+
+
+
+fenetre = Tk()
+fenetre.title('snake')
+fenetre.resizable(False, False)
+
+
+
+
+
+fenetre.mainloop()

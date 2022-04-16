231c066cc8adb5a827d20a8458414dd92460ad9f
Myrmidons-nath <tueronathan@gmail.com>
2022-04-16 15:13:53 +0200

    projet python

--- cours/turtleprojet.py
+++ cours/turtleprojet.py
@@ -1,1 +1,19 @@
-#importation du module 
+import turtle  #importation du module 
+import time #gerer temps
+
+
+""""
+turtle:
+forward > ligne
+left > angle
+circle > cercle
+
+
+
+time:
+sleep() > temps de latence
+
+"""
+
+turtle.circle(100)
+time.sleep(2)

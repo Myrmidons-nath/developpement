669cbcb7fd6222d38b46adb14f86a821e41628c6
Myrmidons-nath <tueronathan@gmail.com>
2022-04-20 14:25:50 +0200

    python

--- reconnaissance/face/projet_poo.py
+++ reconnaissance/face/projet_poo.py
@@ -8,7 +8,7 @@ class detections_face:
     def __init__(self):
         self.mp_face = mp.solutions.face_detection
         self.mp_draw = mp.solutions.drawing_utils
-        self.face_detection = self.mp_face.FaceDetection()
+        self.face_detection = self.mp_face.FaceDetection(0.75)
     def detect(self, frame):
         framrgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         results = self.face_detection.process(framrgb)
@@ -18,7 +18,8 @@ class detections_face:
                 box = detection.location_data.relative_bounding_box
                 a, b, c= frame.shape
                 bbox = int(box.xmin * b), int(box.ymin * a), int(box.width * b), int(box.height * a)
-                cv2.rectangle(frame, bbox, (0, 255, 0), 2)
+                cv2.rectangle(frame, bbox, (255, 255, 0), 2)
+                cv2.putText (frame, f'{int (detection.score[0] * 100)}%', (bbox[0], bbox [1] - 20), cv2.FONT_HERSHEY_PLAIN, 3, (255, 255, 0), 2)
 
 def main():
         
